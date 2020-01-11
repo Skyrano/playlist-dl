8e52fc9efccde69e872e2b5c3198d43385691c92
Skyrano <alistair-rameau@hotmail.fr>
2020-01-11 17:57:24 +0100

    Changement de la liste et de la méthode d'exclusion

--- playlist-dl.py
+++ playlist-dl.py
@@ -6,22 +6,23 @@ if len(sys.argv) == 1:
 else:
         playlist = sys.argv[1]
 
-exclude_list = {" lyrics"," lyric"," (lyrics)"," ENG Sub", " VIDEO", " Official", " (official music video)", " official",
-                " (lyric)"," ENG SUB"," Subtitles", " [Lyrics]", " [ Lyrics ]", " MUSIC", " (official)", " (Official)",
-                " Lyrics"," Lyric"," (Lyrics)"," (English Subtitles)", " (Official Music Video)", " (Music Video)",
-                " (Lyric)"," English Subtitles"," (Subtitles)", " Music Video"}
+#" Lyric"," lyric"," (Lyric)"," (official music video)"," (lyric)", " [Lyrics]"," [ Lyrics ]", " (official)", " (Official)"," (lyrics)"," (Lyrics)"," (English Subtitles)", " (Official Music Video)", " (Music Video)"," (Subtitles)",   
+exclude_list = {" - Lyrics"," Lyrics"," lyrics"," ENG Sub", " VIDEO", " Official", " official",
+                " ENG SUB"," Subtitles"," MUSIC"," English Subtitles"," Music Video"}
 
 def Clean(filename):
         filename = filename.replace("_", " ")
         filename = filename[0:-16] + filename[-4:]
         for word in exclude_list:
                 filename = filename.replace(word, "") 
+                while filename[-5] in [" ","-","."]:
+                        filename = filename[0:-5] + filename[-4:]
         return filename
         
-os.chdir("/home/alistair/Musique/")
-os.system("youtube-dl -x --audio-format mp3 --yes-playlist --download-archive /home/alistair/Musique/music-list --restrict-filenames {}".format(playlist))
+os.chdir("/home/skyrano/Musique/")
+os.system("youtube-dl -x --audio-format mp3 --yes-playlist --download-archive /home/skyrano/Musique/music-list --restrict-filenames {}".format(playlist))
 
-for file in os.listdir("/home/alistair/Musique/"):
+for file in os.listdir("/home/skyrano/Musique/"):
         if file.endswith(".mp3"):
                 title = Clean(file)
-                os.system("mv '/home/alistair/Musique/{}' '/home/alistair/Musique/ReadyToPhone/{}'".format(file,title))
+                os.system("mv '/home/skyrano/Musique/{}' '/home/skyrano/Musique/ReadyToPhone/{}'".format(file,title))
